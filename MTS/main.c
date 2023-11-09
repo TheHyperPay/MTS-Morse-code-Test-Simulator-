74cf32260f9d2f1c5ec15148a48b0939458554ed
TheHyperPay <kimsihoon@proton.me>
2023-11-09 19:23:52 +0900

    Fix Random Function

--- MTS/main.c
+++ MTS/main.c
@@ -9,9 +9,10 @@
 
 #define SAFE_DELETE(ptr)                do { free(ptr); (ptr) = NULL; } while(0)
 #define CAPPING(char_array)             strupr(char_array)
-#define DELETE_SLASH_N(text_array)      text_array[strcspn(text_array, "\n")] = 0;
+#define DELETE_SLASH_N(text_array)      text_array[strcspn(text_array, "\n")] = 0
 
 #define WORD_MAX                        20
+
 typedef unsigned int                    fileLine_t;
 
 typedef struct {
@@ -73,9 +74,18 @@ char convertM2A(const char* morseCode)
     return ' ';
 }
 
+
+#define TIME_NOW                        (unsigned int)time(NULL)
+#define SHUFFLE_BIT_1                   0b10101010101010101010101010101010
+#define SHUFFLE_BIT_2                   0b01010000100001000010000100001010
+
 int randomDrop(unsigned short maxVal)
 {
-    srand((unsigned int)time(NULL));
+    srand((SHUFFLE_BIT_1 >> 1 ^ TIME_NOW << 8) >> 10 ^
+        (SHUFFLE_BIT_2 << 1 & TIME_NOW >> 3) >> 10 ^
+        (SHUFFLE_BIT_1 << 0 ^ TIME_NOW << 15) >> 3 ^
+        (SHUFFLE_BIT_2 >> 1 & TIME_NOW << 3) << 10 ^
+        (SHUFFLE_BIT_1 >> 1 ^ TIME_NOW >> 11) >> 1);
     return (rand() % maxVal + 1);
 }
 
@@ -84,6 +94,7 @@ wordText_t getFileTexts(const char* fileName, fileLine_t fileLine)
 {
     FILE* fp = fopen(fileName, "r");
     int counter = randomDrop(fileLine);
+    //printf("%d: ", counter);
     wordText_t buffer;
     if (fp == NULL)
     {
@@ -114,16 +125,14 @@ fileLine_t getFileLine(const char* fileName)
     char buffer[20];
     fileLine_t lines = 0;
 
-    if (fp == NULL) {
+    if (fp == NULL)
         return 1;
-    }
 
     while (fgets(buffer, sizeof(buffer), fp)) {
         lines++;
     }
 
     fclose(fp);
-
     return lines;
 }
 
@@ -136,15 +145,6 @@ fileLine_t getFileLine(const char* fileName)
 #define SND_DOT_RCV						{Beep(SND_FREQUENCY, SND_GAP * 1);	Sleep(SND_SEP_CODE);}
 #define SND_END_OF_LETTER				Sleep(SND_SEP_LETTER);
 #define SND_END_OF_WORD					Sleep(SND_SEP_WORD);
-
-#define TT_1 200
-#define TEST_MUSIC_1                    {Beep(262, TT_1);Sleep(TT_1);Beep(262, TT_1);Sleep(TT_1);Beep(247, TT_1*3);Beep(262, TT_1);Sleep(TT_1*2);Beep(294, TT_1*2);Beep(330, TT_1);Sleep(TT_1*3);\
-                                        Beep(349, TT_1*2);Beep(330, TT_1*2);Sleep(TT_1*2);Beep(294, TT_1*4);Beep(262, TT_1*2);Beep(247, TT_1*4);}
-#define TT2 250
-#define TEST_MUSIC_2 {Beep(659, TT2*8); Beep(587, TT2*3);Beep(554, TT2*3);Beep(659, TT2*2);Beep(587, TT2*8);Beep(587, TT2*3);Beep(523, TT2*3);Beep(466, TT2*2);\
-                     Beep(440, TT2*7);Beep(415, TT2*1);Beep(440, TT2*5);Beep(329, TT2*1);Beep(415, TT2*1);Beep(440, TT2*1);Beep(493, TT2*5);Beep(329, TT2*1);Beep(415, TT2*1);Beep(493, TT2*1);Beep(554, TT2*4);Beep(493, TT2*4);\
-                     Beep(659, TT2*8);Beep(587, TT2*3);Beep(554, TT2*3);Beep(493, TT2*2);Beep(440, TT2*7);Beep(440, TT2/2);Beep(415, TT2/2);Beep(440, TT2*1.5);Beep(392, TT2*1.5);Beep(349, TT2*2);Beep(392, TT2*1);Beep(349, TT2*2);\
-                     Beep(329, TT2*7);Beep(415, TT2*1);Beep(440, TT2*4);Beep(329, TT2*1.5);Beep(293, TT2*1.5);Beep(277, TT2*1);Beep(293, TT2*8);Beep(293, TT2*1.5);Beep(277, TT2*1.5);Beep(493/2, TT2*3);Beep(415/2, TT2*2);Beep(440/2, TT2*8);}
 
 
 void morseSound(const char* message) {
@@ -164,7 +164,6 @@ void morseSound(const char* message) {
         SND_END_OF_LETTER
     }
 }
-
 
 
 void runProgram1()
@@ -188,11 +187,16 @@ void runProgram1()
 #define WORD_FILE_PATH                  "EnglishWords.data"
 #define WORD_FILE_LINE                  wordFile_line
 
+void init();
 int main()
 {
 	int programCode = 0;
-    wordFile_line = getFileLine(WORD_FILE_PATH);
+    init();
     
+    printf("%s\n", TEXT(getFileTexts(WORD_FILE_PATH, WORD_FILE_LINE)));
+    Sleep(1200);
+    printf("%s\n", TEXT(getFileTexts(WORD_FILE_PATH, WORD_FILE_LINE)));
+    Sleep(1200);
     printf("%s\n", TEXT(getFileTexts(WORD_FILE_PATH, WORD_FILE_LINE)));
     Sleep(1200);
     printf("%s\n", TEXT(getFileTexts(WORD_FILE_PATH, WORD_FILE_LINE)));
@@ -218,3 +222,27 @@ ProgramStart_Point:
 
 	goto ProgramStart_Point;
 }
+
+void init()
+{
+    wordFile_line = getFileLine(WORD_FILE_PATH);
+}
+
+
+
+
+
+
+
+
+
+
+
+#define TT_1 200
+#define TEST_MUSIC_1                    {Beep(262, TT_1);Sleep(TT_1);Beep(262, TT_1);Sleep(TT_1);Beep(247, TT_1*3);Beep(262, TT_1);Sleep(TT_1*2);Beep(294, TT_1*2);Beep(330, TT_1);Sleep(TT_1*3);\
+                                        Beep(349, TT_1*2);Beep(330, TT_1*2);Sleep(TT_1*2);Beep(294, TT_1*4);Beep(262, TT_1*2);Beep(247, TT_1*4);}
+#define TT2 250
+#define TEST_MUSIC_2 {Beep(659, TT2*8); Beep(587, TT2*3);Beep(554, TT2*3);Beep(659, TT2*2);Beep(587, TT2*8);Beep(587, TT2*3);Beep(523, TT2*3);Beep(466, TT2*2);\
+                     Beep(440, TT2*7);Beep(415, TT2*1);Beep(440, TT2*5);Beep(329, TT2*1);Beep(415, TT2*1);Beep(440, TT2*1);Beep(493, TT2*5);Beep(329, TT2*1);Beep(415, TT2*1);Beep(493, TT2*1);Beep(554, TT2*4);Beep(493, TT2*4);\
+                     Beep(659, TT2*8);Beep(587, TT2*3);Beep(554, TT2*3);Beep(493, TT2*2);Beep(440, TT2*7);Beep(440, TT2/2);Beep(415, TT2/2);Beep(440, TT2*1.5);Beep(392, TT2*1.5);Beep(349, TT2*2);Beep(392, TT2*1);Beep(349, TT2*2);\
+                     Beep(329, TT2*7);Beep(415, TT2*1);Beep(440, TT2*4);Beep(329, TT2*1.5);Beep(293, TT2*1.5);Beep(277, TT2*1);Beep(293, TT2*8);Beep(293, TT2*1.5);Beep(277, TT2*1.5);Beep(493/2, TT2*3);Beep(415/2, TT2*2);Beep(440/2, TT2*8);}
